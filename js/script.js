900129b1f21be800348b2281c648b6d6298e6e8f
J. Doig <33632968+eldoigo@users.noreply.github.com>
2019-06-25 14:53:38 +0100

    ver1
    ver1

---
+++ js/script.js
@@ -0,0 +1,66 @@
+ // JavaScript Document
+
+// THE AMAZING DISAPPEARING NAV EFFECT - REAPPEAR ON SCROLL 300 TOP
+   $(window).resize(function() {
+'use strict';
+   $("wrapper").height($(window).height());
+   });
+
+   (function ($) {
+     $(document).ready(function(){
+       $(function () {
+         $(window).scroll(function () {
+           if ($(this).scrollTop() > 600) {
+              $('.nav-right').fadeOut();
+           } else {
+              $('.nav-right').fadeIn();}
+       });
+    });   	   
+});   
+}(jQuery));
+
+
+	
+	$("#loader").delay(1800).fadeOut("1000");
+
+//FONT & IMG FADE IN
+
+$('header').delay(300).fadeIn(500).removeClass('hidden');
+$('.hero-holder').delay(300).fadeIn(500).removeClass('hidden');
+
+
+//	$('header').delay(2600).fadeIn(1000).removeClass('hidden');
+//	$('.hero-holder').delay(3000).fadeIn(1500).removeClass('hidden');
+
+// Smooth scroll effect by ancoring
+
+$('a').click(function(){
+	'use strict';
+	$('html, body').animate({
+		scrollTop: $( $.attr(this, 'href') ).offset().top
+	}, 1200);
+	return false;
+});
+// Responsive menu
+
+	
+	$('.resp-men-but').click(function(){
+		$('.resp-menu').fadeToggle(600);
+	});
+	
+	'use strict';
+	$('.exit').click(function(){
+		$('.resp-menu').fadeOut(800);
+	});
+	
+// responsive menu fade out on click
+
+	
+	$('.sub1').click(function(){
+		$('.resp-menu').fadeOut(600);
+	});
+	
+
+
+
+
