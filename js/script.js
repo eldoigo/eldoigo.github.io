3677d881b88b5700d66ffabe6823393201db962d
J. Doig <33632968+eldoigo@users.noreply.github.com>
2019-06-30 11:43:50 +0100

    ver15
    ver15

--- js/script.js
+++ js/script.js
@@ -26,8 +26,8 @@
 //FONT & IMG FADE IN
 
 
-$('header').delay(2600).fadeIn(1000).removeClass('hidden');
-$('.hero-holder').delay(3000).fadeIn(1500).removeClass('hidden');
+$('header').delay(260).fadeIn(100).removeClass('hidden');
+$('.hero-holder').delay(300).fadeIn(150).removeClass('hidden');
 
 //$('header').delay(200).fadeIn(100).removeClass('hidden');
 // $('.hero-holder').delay(220).fadeIn(150).removeClass('hidden');
